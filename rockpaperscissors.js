f9a113e93a32e210427ba7696b2a9eaa189af04e
Kevin Mozzillo <kevinmozzillo@gmail.com>
2024-03-09 18:56:51 -0500

    Created playRound() function which returns a string declaring the winner of a round of rock, paper, scissors.

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -15,5 +15,43 @@ function getComputerChoice() {
     return choice;
 };
 
+function playRound(playerSelection, computerSelection) {
+    switch(playerSelection.toLowerCase()) {
+        case 'rock':
+            if (computerSelection === 'Rock') {
+                return 'Tie!';
+            }
+            if (computerSelection === 'Paper') {
+                return 'You Lose! Paper beats rock!';
+            }
+            else {
+                return 'You Win! Rock beats scissors!';
+            };
+        case 'paper':
+            if (computerSelection === 'Rock') {
+                return 'You Win! Paper beats rock!';
+            }
+            if (computerSelection === 'Paper') {
+                return 'Tie!';
+            }
+            else {
+                return 'You Lose! Scissor beats paper!';
+            }
+        case 'scissor':
+            if (computerSelection === 'Rock') {
+                return 'You lose! Rock beats scissor!';
+            }
+            if (computerSelection === 'Paper') {
+                return 'You win! Scissor beats paper!';
+            }
+            else {
+                return 'Tie';
+            }
+    }
+}
 
-console.log(getComputerChoice());
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playerSelection);
+console.log(computerSelection);
+console.log(playRound(playerSelection, computerSelection));
